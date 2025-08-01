9c990b6b398a4d76f87fe6976bbf91afb87793e8
nishad <nishadkoormath@gmail.com>
2025-08-01 17:32:11 +0530

    created accounts - views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class userRegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+        )
+        return user
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = userRegisterSerializer
+    permission_classes = [AllowAny]
+        
--- tour_package_management/urls.py
+++ tour_package_management/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api/countries/', include('countries.urls')),
     path('api/packages/', include('packages.urls')),
     path('api/enquiries/', include('enquiries.urls')),
+    path('api/accounts/', include('accounts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
